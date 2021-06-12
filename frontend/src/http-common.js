486771a58b024ceb07d70b9d7a56637333f0ff1b
malcolmjefferson <malcolmj0415@gmail.com>
2021-06-12 00:51:37 -0700

    Realm changes

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     //set default params. all routes come after this.
-    baseURL: "http://localhost:5000/api/v1/restaurants",
+    baseURL: "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restaurant-reviews-gscli/service/restaurants/incoming_webhook/",
     headers: {
         "Content-type": "application/json"
     }
